36e84a8a36c4d8938082966bf818524cc6a49b63
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-03-27 18:53:01 -0500

    Added functions to restore a SQL database & retrieve the file list from a backup file

---
+++ MrSQL/Get-MrSqlDbRestoreFileList.ps1
@@ -0,0 +1,24 @@
+﻿function Get-MrSqlDbRestoreFileList {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ServerInstance,
+
+        [Parameter(ValueFromPipeline)]
+        [string]$BackupFilePath
+    )
+
+    Invoke-Sqlcmd -ServerInstance $ServerInstance -Database master -QueryTimeout 120 –Query "
+    RESTORE FILELISTONLY
+    FROM DISK = '$BackupFilePath' WITH FILE = 1
+    GO" |
+    Select-Object -Property LogicalName, PhysicalName, @{label='Type';expression={
+        switch ($_.Type) {
+            'L' {'Log'}
+            'D' {'Data'}
+            'F' {'FullTextCatalog'}
+            'S' {'Other'}
+        }
+    }}
+
+}
---
+++ MrSQL/Restore-MrSqlDatabase.ps1
@@ -0,0 +1,42 @@
+﻿function Restore-MrSqlDatabase {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ServerInstance,
+
+        [Parameter(Mandatory)]
+        [string]$Database,
+
+        [string]$DatabasePath,
+
+        [Parameter(ValueFromPipeline)]
+        [string[]]$BackupFilePath        
+    )
+
+    PROCESS {
+        $FileList = Get-MrSqlDbRestoreFileList -ServerInstance $ServerInstance -BackupFilePath $BackupFilePath[0]
+        
+        if (-not($PSBoundParameters.DatabasePath)) {
+            $DatabasePath = Split-Path -Parent ($FileList | Where-Object type -eq data | Select-Object -ExpandProperty PhysicalName)
+        }
+
+        foreach ($file in $BackupFilePath) {
+            Invoke-Sqlcmd -ServerInstance $ServerInstance -Database master -QueryTimeout 3600 –Query "
+            RESTORE DATABASE $Database
+            FROM  DISK = N'$file'
+            WITH  FILE = 1,
+             MOVE N'$($FileList | Where-Object Type -eq data | Select-Object -ExpandProperty LogicalName)' TO N'$(Join-Path -Path $DatabasePath -ChildPath "$Database.mdf")',
+             MOVE N'$($FileList | Where-Object Type -eq log | Select-Object -ExpandProperty LogicalName)' TO N'$(Join-Path -Path $DatabasePath -ChildPath "$($Database)_log.ldf")',
+             NORECOVERY, REPLACE
+            GO"
+        }
+    }
+
+    END {
+        Invoke-Sqlcmd -ServerInstance $ServerInstance -Database master -QueryTimeout 600 –Query "
+        RESTORE DATABASE $Database
+        WITH RECOVERY
+        GO"
+    }
+
+}
