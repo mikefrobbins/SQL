9a3faab9aacf999ddc27ddd3b4cddb2fb4d24b35
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-11-22 13:07:48 -0600

    Added logic for multiple jobs and pipeline input

--- MrSQL/Get-MrSQLAgentJobStatus.ps1
+++ MrSQL/Get-MrSQLAgentJobStatus.ps1
@@ -1,13 +1,13 @@
 ﻿#Requires -Version 3.0
-function Get-MrSQLAgentJobStatus {
+function Get-MrSqlAgentJobStatus {
 
 <#
 .SYNOPSIS
-    Retrieves the status of the specified SQL Agent Job from the specified target instance of SQL Server.
+    Retrieves the status of one or more SQL Agent Jobs from the specified target instance of SQL Server.
  
 .DESCRIPTION
-    Get-MrSQLAgentJobStatus is a PowerShell function that is designed to retrieve the status of the specified SQL
-    Server Agent job from the specified target instance of SQL Server without requiring the SQL Server PowerShell
+    Get-MrSqlAgentJobStatus is a PowerShell function that is designed to retrieve the status of one or more SQL
+    Server Agent jobs from the specified target instance of SQL Server without requiring the SQL Server PowerShell
     module or snap-in to be installed.
  
 .PARAMETER ServerInstance
@@ -15,20 +15,23 @@ function Get-MrSQLAgentJobStatus {
     specify the computer name: MyComputer. For named instances, use the format ComputerName\InstanceName.
  
 .PARAMETER Name
-    Specifies the name of the Job object that this cmdlet gets. The name may or may not be
+    Specifies the name of one or more Job objects that this cmdlet gets. The names may or may not be
     case-sensitive, depending on the collation of the SQL Server where the SQL Agent is running.
 
 .PARAMETER Credential
     SQL Authentication userid and password in the form of a credential object.
  
 .EXAMPLE
-     Get-MrSQLAgentJobStatus -ServerInstance SQLServer01 -Name syspolicy_purge_history
+     Get-MrSqlAgentJobStatus -ServerInstance SQLServer01 -Name syspolicy_purge_history, test
 
 .EXAMPLE
-     Get-MrSQLAgentJobStatus -ServerInstance SQLServer01 -Name syspolicy_purge_history -Credential (Get-Credential)
+     Get-MrSqlAgentJobStatus -ServerInstance SQLServer01 -Name syspolicy_purge_history -Credential (Get-Credential)
+
+.EXAMPLE
+     'syspolicy_purge_history', 'test' | Get-MrSqlAgentJobStatus -ServerInstance SQLServer01
 
 .INPUTS
-    None
+    String
  
 .OUTPUTS
     PSCustomObject
@@ -44,44 +47,56 @@ function Get-MrSQLAgentJobStatus {
         [Parameter(Mandatory)]
         [string]$ServerInstance,
 
-        [Parameter(Mandatory)]
-        [string]$Name,
+        [Parameter(Mandatory,
+                   ValueFromPipeLine)]
+        [string[]]$Name,
 
         [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    $Params = @{
-        ServerInstance = $ServerInstance
-        Database = 'msdb'
-        Query = "EXEC dbo.sp_help_job @JOB_NAME = '$Name', @job_aspect= 'JOB'"
+    BEGIN {
+    
+        $Params = @{
+            ServerInstance = $ServerInstance
+            Database = 'msdb'
+        }
+
+        if ($PSBoundParameters.Credential) {
+            $Params.Credential = $Credential
+        }
+
     }
 
-    if ($PSBoundParameters.Credential) {
-        $Params.Credential = $Credential
-    }
+    PROCESS {
+        foreach ($n in $Name) {
+        
+            $Params.Query = "EXEC dbo.sp_help_job @JOB_NAME = '$n', @job_aspect= 'JOB'"
+            $Results = Invoke-MrSqlDataReader @Params |
+                       Select-Object -Property originating_server, name, last_run_outcome, current_execution_status
 
-    $Results = Invoke-MrSqlDataReader @Params |
-    Select-Object -Property originating_server, name, last_run_outcome, current_execution_status
+            [pscustomobject]@{
+                ComputerName = $Results.originating_server
+                Name = $Results.name
+                Outcome = switch ($Results.current_execution_status) {
+                              1 {'Executing';break}
+                              2 {'Waiting for thread';break}
+                              3 {'Between retries';break}
+                              4 {'Idle';break}
+                              5 {'Suspended';break}
+                              7 {'Performing completion actions';break}        
+                              default {'An unknown error has occurred'}
+                          }
+                Status = switch ($Results.last_run_outcome) {
+                             0 {'Failed';break}
+                             1 {'Succeeded';break}
+                             3 {'Canceled';break}
+                             5 {'Unknown';break}     
+                             default {'An unknown error has occurred'}
+                         }
+            }
+        
+        }
 
-    [pscustomobject]@{
-        ComputerName = $Results.originating_server
-        Name = $Results.name
-        Outcome = switch ($Results.current_execution_status) {
-                      1 {'Executing';break}
-                      2 {'Waiting for thread';break}
-                      3 {'Between retries';break}
-                      4 {'Idle';break}
-                      5 {'Suspended';break}
-                      7 {'Performing completion actions';break}        
-                      default {'An unknown error has occurred'}
-                  }
-        Status = switch ($Results.last_run_outcome) {
-                     0 {'Failed';break}
-                     1 {'Succeeded';break}
-                     3 {'Canceled';break}
-                     5 {'Unknown';break}     
-                     default {'An unknown error has occurred'}
-                 }
     }
 
 }
