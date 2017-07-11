7233682d9aea95770beebf61c2e999a0acdcce7e
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2017-07-11 16:10:10 -0500

    Added Get-MrSqlVersion function

---
+++ MrSQL/Get-MrSqlVersion.ps1
@@ -0,0 +1,47 @@
+﻿#Requires -Version 3.0
+function Get-MrSqlVersion {
+    
+    [CmdletBinding()]
+    param ( 
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(ValueFromPipeline)] 
+        [string]$ServerInstance =$env:COMPUTERNAME
+ 
+    ) 
+    
+    BEGIN {
+        [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.Smo') | Out-Null
+    }
+
+    PROCESS{
+        foreach ($Instance in $ServerInstance) {
+            $Server = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Server' -ArgumentList $ServerInstance
+
+            try {
+                $VersionName = switch ($Server.VersionString.SubString(0,4)) {
+                        '13.0' {'SQL Server 2016'; break}  
+                        '12.0' {'SQL Server 2014'; break}  
+                        '11.0' {'SQL Server 2012'; break}  
+                        '10.5' {'SQL Server 2008 R2'; break}  
+                        '10.0' {'SQL Server 2008'; break} 
+                        '13.0' {'SQL Server 2016 SP1'; break}  
+                        '12.0' {'SQL Server 2014 SP2'; break}  
+                        '11.0' {'SQL Server 2012 SP3'; break}  
+                        '10.5' {'SQL Server 2008 R2 SP3'; break}  
+                        '10.0' {'SQL Server 2008 SP4'; break}  
+                }
+            }
+            catch {
+                Write-Warning -Message "Unable to connect to SQL Instance: $Instance"
+                Continue
+            }
+
+            [pscustomobject]@{
+                ComputerName = $Server.Name
+                VersionName = $VersionName
+                Version = $Server.VersionString
+            }
+        }
+    }
+}
