23db134c500e481609f1950749339c7f564f8484
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2019-07-25 15:55:27 -0500

    Moved functions to private/public folder structure

--- MrSQL/MrSQL.psm1
+++ MrSQL/MrSQL.psm1
@@ -1,5 +1,5 @@
-﻿#Dot source all functions in all ps1 files located in the module folder
-Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 |
+﻿#Dot source all functions in all ps1 files located in the module's public and private folders, excluding tests and profiles.
+Get-ChildItem -Path $PSScriptRoot\public\*.ps1, $PSScriptRoot\private\*.ps1 -Exclude *.tests.ps1, *profile.ps1 -ErrorAction SilentlyContinue |
 ForEach-Object {
     . $_.FullName
 }
